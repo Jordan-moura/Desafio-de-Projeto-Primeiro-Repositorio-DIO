b7a1b7d8a1189d58b619fb83d3a93fb1ada8448d
Jordan-moura <jordan.moura16@gmail.com>
2022-06-26 21:21:40 -0300

    Pos Aula - Desenvolvimento com mapOf

---
+++ Kotlin/Orientacao-a-Objetos/Collections/api-collections/src/main/kotlin/one/jordan/collections/Funcionario.kt
@@ -0,0 +1,13 @@
+package one.jordan.collections
+
+data class Funcionario(
+    val nome: String,
+    val salario: Double,
+    val tipoContracao: String
+){
+    override fun toString(): String =
+        """
+           Nome:    $nome
+           Salario: $salario
+       """.trimIndent()
+}
--- Kotlin/Orientacao-a-Objetos/Collections/api-collections/src/main/kotlin/one/jordan/collections/TesteList.kt
+++ Kotlin/Orientacao-a-Objetos/Collections/api-collections/src/main/kotlin/one/jordan/collections/TesteList.kt
@@ -20,15 +20,3 @@ fun main() {
     println("-------------------------------")
     funcionarios.groupBy{it.tipoContracao}.forEach{println(it)}
 }
-
-data class Funcionario(
-    val nome: String,
-    val salario: Double,
-    val tipoContracao: String
-    ){
-    override fun toString(): String =
-       """
-           Nome:    $nome
-           Salario: $salario
-       """.trimIndent()
-}
