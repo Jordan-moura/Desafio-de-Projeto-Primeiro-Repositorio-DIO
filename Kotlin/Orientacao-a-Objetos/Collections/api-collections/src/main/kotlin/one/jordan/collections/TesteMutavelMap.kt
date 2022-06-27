8a2fdfc8d5c7e8fbf6fe8a52fda77836991eb20d
Jordan-moura <jordan.moura16@gmail.com>
2022-06-27 00:27:08 -0300

    Terminando a materia - Aplicando Conceitos de Colecoes, Arrays e Listas

---
+++ Kotlin/Orientacao-a-Objetos/Collections/api-collections/src/main/kotlin/one/jordan/collections/TesteMutavelMap.kt
@@ -0,0 +1,22 @@
+package one.jordan.collections
+
+fun main() {
+    val joao = Funcionario(nome = "Joao", salario = 2000.0, tipoContracao = "CLT")
+    val pedro = Funcionario(nome = "Pedro", salario = 1500.0, tipoContracao = "PJ")
+    val maria = Funcionario(nome = "Maria", salario = 4000.0, tipoContracao = "CLT")
+
+    val repositorio = Repositorio<Funcionario>()
+
+    repositorio.create(joao.nome, joao)
+    repositorio.create(pedro.nome, pedro)
+    repositorio.create(maria.nome, maria)
+
+    println(repositorio.findById(joao.nome))
+
+    println("------------------------------------")
+    repositorio.findAll().forEach{ println(it) }
+
+    println("------------------------------------")
+    repositorio.remove(maria.nome)
+    repositorio.findAll().forEach{ println(it) }
+}
